13723e6bb1e8f33565a85a85a96b15e81b6511ea
DonnieU <thedonnieutley@gmail.com>
2018-06-26 15:27:27 -0500

    Added Personal Bookmarks; also choices for 'url' and 'folder' bookmark types

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Bookmark(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
   name = models.CharField(max_length=200)
   url = models.URLField(max_length=200)
-  created_at = models.DateTimeField(auto_now_add=True)
-  last_modified = models.DateTimeField(auto_now=True)
-  location_folder = models.FilePathField(allow_folders=True, default='~')
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  # 'url' or 'folder -- taken from how Chrome delineates in its JSON Bookmarks file
+  bookmark_type = models.CharField(max_length=6, choices=[('u', 'url'), ('f', 'folder')], default='u')
+
+class PersonalBookmark(Bookmark):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
