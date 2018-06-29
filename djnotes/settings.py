794aced2e03d80c963ab9507c65f1166a772ee09
DonnieU <thedonnieutley@gmail.com>
2018-06-29 11:22:17 -0500

    deploying to heroku

--- djnotes/settings.py
+++ djnotes/settings.py
@@ -88,7 +88,8 @@ WSGI_APPLICATION = 'djnotes.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 
 
 # Password validation
